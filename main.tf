dd2d24b96d2c6af2ef02921d9f96dd4e0d9e94ef
Hossam098s <hossam_jamil_1832@fci.helwan.edu.eg>
2024-11-08 01:37:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "azurerm" {
-  features {}
- 
-  subscription_id = "784262c0-253b-469e-96a9-def4349c9c23"
-}
-
 resource "azurerm_resource_group" "webapp_rg" {
   name     = "webapp-resource-group"
   location = "West us 2"
